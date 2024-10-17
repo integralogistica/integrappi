6a52297ada1e02265514665f657c34f585eec2b9
EdwinZarateP <emzp1994@gmail.com>
2024-10-17 13:50:47 -0500

    api2

--- bd/bd_cliente.py
+++ bd/bd_cliente.py
@@ -1,3 +1,4 @@
 from pymongo import MongoClient
 
-bd_cliente = MongoClient("mongodb+srv://emzp1994:Glamping2024*@glampingdb.c6epua0.mongodb.net/?retryWrites=true&w=majority&appName=GLAMPINGDB").glamping
+bd_cliente = MongoClient("mongodb+srv://integra:integra2024@integrappi.su5yi.mongodb.net/?retryWrites=true&w=majority&appName=integrappi").glamping
+# bd_cliente = MongoClient("mongodb+srv://emzp1994:Glamping2024*@glampingdb.c6epua0.mongodb.net/?retryWrites=true&w=majority&appName=GLAMPINGDB").glamping
