0efff1ca3a4cceda04e3a15282a50705c60a543f
EdwinZarateP <emzp1994@gmail.com>
2024-11-14 07:58:57 -0500

    cambio de correo api

--- bd/bd_cliente.py
+++ bd/bd_cliente.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
 
-bd_cliente = MongoClient("mongodb+srv://integra:integra2024@integrappi.su5yi.mongodb.net/?retryWrites=true&w=majority&appName=integrappi")
+# bd_cliente = MongoClient("mongodb+srv://integra:integra2024@integrappi.su5yi.mongodb.net/?retryWrites=true&w=majority&appName=integrappi")
+bd_cliente = MongoClient("mongodb+srv://integra:integra2025@integrappi.agvcg.mongodb.net/?retryWrites=true&w=majority&appName=integrappi")
+                          
                           
